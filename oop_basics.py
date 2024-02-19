06d9fa3bbd79e47c441ecff02ba82de1d1d20609
Rushya Puttam <rputtam6@gmail.com>
2024-02-19 00:49:59 -0600

    adding oop basics .py file

---
+++ oop_basics.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self, input_name,input_breed, input_age):
+        self.name = input_name
+        self.breed = input_breed
+        self_age = input_age
+
+dog_a = Dog('Arya','Husky',3)
+print(dog_a.name)
