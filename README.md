06f30b9deaf6ad5acecb6f1d5af21a5bd71677bb
Rushya Puttam <rputtam6@gmail.com>
2024-02-19 19:42:12 -0600

    oop files addition

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 Concepts of Object Oriented Programming in Python with a project  
 
 This repository has basic oop script in python which has class, objects, attributes and methods
+and code implementing the below concepts
 
 The four important concepts in OOP are
 1. Abstraction
 2. Encapsulation
-3. Inheritane
+3. Inheritance
 4. Polymorphism
 
 Resources:
 1. https://www.codecademy.com/learn/learn-object-oriented-programming-with-python
 2. https://www.coursera.org/learn/object-oriented-programming-and-gui-with-python
+3. https://www.coursera.org/learn/object-oriented-python
--- oop_basics.py
+++ oop_basics.py
@@ -13,10 +13,12 @@ class Person:
         self.name = input_name 
         self.age = input_age
         self.country = input_country
-    
+
+    #custom method
     def update_age(self,i):
         self.age += i
 
+    #setters and getters
     def setCountry(self,country):
         self.country = country
     def getCountry(self):
@@ -26,7 +28,13 @@ class Person:
 	    print("Person name is {name}. Age is {age}. Country is {country}".format(name=self.name,
          age=self.age,country=self.country))
 
-person1 = Person('Ruby',25,'India')
+    def __str__(self): # this gets called when an object of this class is printed
+        return f'{self.name},{self.age},{self.country}'
+
+    def __repr__(self): # this gets called when print of repr(object) is called
+        return f'repr_method calling: {self.name},{self.age},{self.country}'
+
+person1 = Person('Ruby',27,'India')
 person1.display()
 print("updating")
 person1.update_age(0.6)
@@ -34,3 +42,27 @@ person1.setCountry('USA')
 person1.getCountry()
 person1.display()
 
+#Output1
+"""  
+Person name is Ruby. Age is 27. Country is India
+updating
+USA
+Person name is Ruby. Age is 27.6. Country is USA
+"""
+
+# The __dict__ attribute is found in every python object. It stores all the attributes of a class
+print(person1.__dict__)
+print(person1)
+
+#Output2
+"""
+{'name': 'Ruby', 'age': 27.6, 'country': 'USA'}
+Ruby,27.6,USA
+"""
+
+print(repr(person1))
+
+#Output3
+"""
+repr_method calling: Ruby,27.6,USA
+"""
