5c0c5e2790c02a7853a9ec1fe2646f0c00902c9a
Rushya Puttam <rputtam6@gmail.com>
2024-03-01 02:45:30 -0600

    logic changes

--- tictactoe.py
+++ tictactoe.py
@@ -15,61 +15,119 @@ class TictacToe:
         self.play_game()
     
     def display_board(self,board):
+       print('before lsit view')
        list_view=[[board[i,j] for j in range(board.shape[1])] for i in range(board.shape[0])]
+       print('after list view')
        for row in list_view:
             print("|".join(row))
-            print("-"*5)
+            print("-"*(board.shape[1]+3))
+
+    def some_func(self):
+        while(True):
+            board_upd_st=self.update_board(x_choice,'X')
+            if not board_upd_st:
+                print('Regive')
+            self.display_board(self.board)
+            if self.match_check('X'):
+                self.end_game()
+                break
+
+    def player_x_input(self):
+        print('here')
+        x_choice=list(input("\nX) Where do you want to mark your position? Enter row and column number in xy format"))
+        self.update_board(x_choice,'X')
+        #return x_choice
+    
+    def player_o_input(self):
+        o_choice=list(input("\nO) Where do you want to mark your position? Enter row and column number in xy format"))
+        self.update_board(o_choice,'O')
+        #return o_choice
 
     def play_game(self):
         print('play func begining')
         while True:
+            self.player_x_input()
+            self.player_o_input()
+        """
+        while True:
             x_choice=list(input("\nX) Where do you want to mark your position? Enter row and column number in xy format"))
-            self.update_board(x_choice,'X')
-            self.display_board(self.board)
-            if self.match_check('X'):
-                play_again = input("Do you want to play again? Give Y/N")
-                if play_again.upper()=='Y':
-                    self.start_game()
-                else:
-                    print('Hope you have enjoyed the game')
+            #if x_choice[0]<=self.rows and x_choice[-1]<=self.cols
+            if self.board[int(x_choice[0])-1,int(x_choice[-1])-1]==' ':
+                board_upd_st=self.update_board(x_choice,'X')
+                self.display_board(self.board)
+                if self.match_check('X'):
+                    self.end_game()
                     break
-                    
+            else:
+                print('Regive')
+            
+        
+        while True:            
             o_choice=list(input("\nO) Where do you want to mark your position? Enter row and column number in xy format"))
             self.update_board(o_choice,'O')
             self.display_board(self.board)
             if self.match_check('O'):
-                play_again = input("Do you want to play again? Give Y/N")
-                if play_again.upper()=='Y':
-                    self.start_game()
-                else:
-                    print('Hope you have enjoyed the game')
-                    break
-            
+                self.end_game()
+                break
+            """ 
+
+    def end_game(self):
+        while(True):
+            play_again = input("Do you want to play again? Give Y/N")
+            if play_again.upper()=='Y':
+                self.start_game()
+                break
+            elif play_again.upper() not in ['Y','N']:
+                print("Invalid input. Please give y or n")
+            else:
+                print('Hope you have enjoyed the game')
+                break
+                #return False
+                
 
     def update_board(self,player_choice,player):
-        if self.board[int(player_choice[0])-1,int(player_choice[1])-1]==' ':
-            self.board[int(player_choice[0])-1,int(player_choice[1])-1] = player
-        else:
-            print('This position is taken. Please give another position')
+        while True:
+            if self.board[int(player_choice[0])-1,int(player_choice[-1])-1]==' ':
+                self.board[int(player_choice[0])-1,int(player_choice[-1])-1] = player
+                self.display_board(self.board)
+                if self.match_check(player) or self.match_tie_check():
+                    self.end_game()
+                    #break
+            else:
+                print('This position is taken. Please give another position')
+                if player=='X':
+                    self.player_x_input()
+                else:
+                    self.player_o_input()
+            return False
 
     def match_check(self,player):
-        if self.diagonal_check(player) or self.across_check(player) or self.below_check(player):
+        if self.across_check(player) or self.below_check(player) or self.diagonal_check(player):
             return True
         else:
             return False
         
     def match_tie_check(self):
-        pass
+        c=0
+        for r in range(0,len(self.board)):
+            print('rows',len(self.board))
+            for c in range(0,len(self.board[0])):
+                print('cols',len(self.board[0]))
+                if self.board[r,c]!=' ':
+                    c+=1
+        print(c)
+        return c==self.rows*self.cols
+
     
     #Row-wise Match check
     def across_check(self,player):
         for i in range(0,self.board.shape[0]):#row
             c=0
             for j in range(0,self.board.shape[1]):#column
-                if j!=2:
+                if j<self.board.shape[1]-1: #should be 1 less than no.of cols
                     if self.board[i,j] == player and self.board[i,j+1] == player:
                         c+=1
-            if c==self.board.shape[0]-1:
+            if c==self.board.shape[1]-1:
                 print('Player {p} wins'.format(p=player))
                 return True
             else:
@@ -77,11 +135,11 @@ class TictacToe:
     
     # Column-wise Match check
     def below_check(self,player):
-        for i in range(0,self.board.shape[0]):#column
+        for i in range(0,self.board.shape[1]):#column
             d=0
-            for j in range(0,self.board.shape[1]):#row
-                if j!=2:
-                    if self.board[j,i]==player and self.board[j+1,i]==player:
+            for j in range(0,self.board.shape[0]):#row
+                if j<self.board.shape[0]-1: #4-1=3
+                    if self.board[j,i]==player and self.board[j+1,i]==player:#2=3
                         d+=1
             if d==self.board.shape[0]-1:
                 print('Player {p} wins!'.format(p=player))
